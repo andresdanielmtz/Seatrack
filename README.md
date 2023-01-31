0325c2c26e9b7e5851f2630a87e5288f180d534a
Andrés Martínez <autummata@gmail.com>
2023-01-30 22:52:48 -0700

    Started reformatting the <About/> component

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Seatrack
 ## Welcome to the future of Debris Tracking
+### (Currently in development) 
 
-To run this project, do `npm install` and `npm run`. 
+To run this project, use `npm install` and `npm run`. 
--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,33 +1,30 @@
 import React from "react";
 import "./About.css";
 
-// show the title of a div when hovering on it, that follows the mouse
-// https://stackoverflow.com/questions/10607688/how-to-show-a-tooltip-when-hovering-over-a-div
-
-
 function About() {
   return (
     <div className="about-wrap">
-      <h1 id="about-who"> Who are we?</h1>
       <p id="about-content">
-        {" "}
-        We are a group of CS Students, from{" "}
-        <a
-          rel="noopener noreferrer"
-          target="_blank"
-          href="https://www.openstreetmap.org/#map=12/29.0918/-110.9444"
-        >
+        <div id="about-intro">
           {" "}
-          Hermosillo, Sonora,
-        </a>{" "}
-        who want to make the world a better place.
-        <br />
-        <div id="about-info" title="Tooltip for the example image">
-          We want to revolutionize the way people see the amounts of trash we
-          produce and release into the environment, year by year.
+          <h1 id="about-who"> Who are we?</h1>
+          We are a group of CS Students, from{" "}
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://www.openstreetmap.org/#map=12/29.0918/-110.9444"
+          >
+            {" "}
+            Hermosillo, Sonora,
+          </a>{" "}
+          who want to make the world a better place.
+          <br />
+          <div class="columns">
+            <div class="column">This is very cool!</div>
+            <div class="column">This is another cool thing</div>
+          </div>
         </div>
       </p>{" "}
-      {/* This should be more than enough I'd say.*/}
     </div>
   );
 }
