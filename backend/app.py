3d766a74fcad45d0dbbd0b97afdd274e4778e29a
Andrés Martínez <autummata@gmail.com>
2023-02-09 15:16:39 -0700

    Added command to run both start and start-backend

--- backend/app.py
+++ backend/app.py
@@ -14,4 +14,4 @@ def data():
     }
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port = 5000, debug=True)
--- src/components/map/Mapping.js
+++ src/components/map/Mapping.js
@@ -22,7 +22,6 @@ const MapComponent = () => {
       }
     )
   }, [])
-
   console.log(profileData.img)
   console.log(profileData.coords)
 
